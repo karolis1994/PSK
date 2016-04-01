ea7d939245470db7dc83d285c8dc91ca9ba9008a
Henrikas <gyngga@gmail.com>
2016-04-01 12:41:38 +0300

    Refactored HouseFacade

--- src/main/Java/DT/Facades/HouseFacade.java
+++ src/main/Java/DT/Facades/HouseFacade.java
@@ -6,12 +6,7 @@
 package DT.Facades;
 
 import DT.Entities.Houses;
-import java.io.Serializable;
-import java.util.List;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 /**
  *
@@ -19,38 +14,5 @@ import javax.persistence.Query;
  */
 @Stateless
 public class HouseFacade extends GenericFacade<Houses> {
-        
-    /*@PersistenceContext(unitName = "DT_DT.ReservationSystem_war_1.0-SNAPSHOTPU")
-    private EntityManager em;
 
-    public void create(Houses house) {
-        em.persist(house);
-    }
-
-    public void edit(Houses house) {
-        em.merge(house);
-    }
-
-    public void remove(Houses house) {
-        em.remove(em.merge(house));
-    }
-
-    public Houses find(Object id) {
-        return em.find(Houses.class, id);
-    }
-
-    public List<Houses> findAll() {
-        return em.createQuery("select object(o) from Houses as o").getResultList();
-    }
-
-    public List<Houses> findRange(int maxResults, int firstResult) {
-        Query q = em.createQuery("select object(o) from Houses as o");
-        q.setMaxResults(maxResults);
-        q.setFirstResult(firstResult);
-        return q.getResultList();
-    }
-
-    public int getItemCount() {
-        return ((Long) em.createQuery("select count(o) from Houses as o").getSingleResult()).intValue();
-    }*/
 }
