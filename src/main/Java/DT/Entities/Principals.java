9e104d5b39d92d1a0c671d5f4eeed422ed4a1676
karolis1994 <karolispock@gmail.com>
2016-05-06 17:49:50 +0300

    Added recommendations

--- src/main/Java/DT/Entities/Principals.java
+++ src/main/Java/DT/Entities/Principals.java
@@ -6,6 +6,7 @@
 package DT.Entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
@@ -21,13 +22,16 @@ import javax.persistence.Table;
 import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author Aurimas
+ * @author Karolis
  */
 @Entity
 @Table(name = "principals")
+@XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "Principals.findAll", query = "SELECT p FROM Principals p"),
     @NamedQuery(name = "Principals.findById", query = "SELECT p FROM Principals p WHERE p.id = :id"),
@@ -43,6 +47,12 @@ import javax.validation.constraints.Size;
     @NamedQuery(name = "Principals.findByIsdeleted", query = "SELECT p FROM Principals p WHERE p.isdeleted = :isdeleted"),
     @NamedQuery(name = "Principals.findByVersion", query = "SELECT p FROM Principals p WHERE p.version = :version")})
 public class Principals implements Serializable {
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "senderid")
+    private Collection<Recommendations> recommendationsCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recieverid")
+    private Collection<Recommendations> recommendationsCollection1;
+    
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -89,7 +99,7 @@ public class Principals implements Serializable {
     private List<Invitations> invitationsList;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "principals1")
     private List<Invitations> invitationsList1;
-
+    
     public Principals() {
     }
 
@@ -200,7 +210,7 @@ public class Principals implements Serializable {
     public void setVersion(int version) {
         this.version = version;
     }
-
+    
     public List<Invitations> getInvitationsList() {
         return invitationsList;
     }
@@ -216,7 +226,7 @@ public class Principals implements Serializable {
     public void setInvitationsList1(List<Invitations> invitationsList1) {
         this.invitationsList1 = invitationsList1;
     }
-
+    
     @Override
     public int hashCode() {
         int hash = 0;
@@ -240,6 +250,24 @@ public class Principals implements Serializable {
     @Override
     public String toString() {
         return "DT.Entities.Principals[ id=" + id + " ]";
+    }
+    
+    @XmlTransient
+    public Collection<Recommendations> getRecommendationsCollection() {
+        return recommendationsCollection;
+    }
+
+    public void setRecommendationsCollection(Collection<Recommendations> recommendationsCollection) {
+        this.recommendationsCollection = recommendationsCollection;
+    }
+
+    @XmlTransient
+    public Collection<Recommendations> getRecommendationsCollection1() {
+        return recommendationsCollection1;
+    }
+
+    public void setRecommendationsCollection1(Collection<Recommendations> recommendationsCollection1) {
+        this.recommendationsCollection1 = recommendationsCollection1;
     }
     
 }
