0c154bff8adbf3313b1d67fef8c08c3827b2bd88
karolis1994 <karolispock@gmail.com>
2016-05-20 15:16:50 +0300

    Fixed user profile viewing, added needed fields.

--- src/main/Java/DT/Beans/UserProfileBean.java
+++ src/main/Java/DT/Beans/UserProfileBean.java
@@ -8,30 +8,38 @@ package DT.Beans;
 import DT.Entities.Principals;
 import DT.Facades.PrincipalsFacade;
 import DT.Facades.SettingsFacade;
+import java.text.SimpleDateFormat;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Karolis
  */
 @ManagedBean(name="userProfileBean")
-@ViewScoped
+@RequestScoped
 public class UserProfileBean {  
         
-    private Principals loggedInPrincipal;
+    // Fields ------------------------------------------------------------------
+
+    private Principals shownPrincipal;
     @EJB
     private PrincipalsFacade principalsFacade;
     @EJB 
     private SettingsFacade settingsFacade;
     
+    @ManagedProperty(value="#{param.userID}")
+    private String userID;
     private String firstname;
     private String lastname;
     private String birthdate;
+    private String memberUntil;
     private String email;
     private String phoneNumber;
     private String address;
@@ -40,101 +48,99 @@ public class UserProfileBean {
     private boolean aboutField;
     private boolean pictureField;
     
+    // Methods -----------------------------------------------------------------
+    
     @PostConstruct
     public void init(){
-        //užkrauname nustatymus
+        //Loading settings
         aboutField = "true".equals(settingsFacade.getSettingByName("AboutField").getSettingvalue());
         //pictureField = "true".equals(settingsFacade.getSettingByName("PictureField").getSettingvalue());
         
-        //gauname prisijungusio vartotojo objektą
-        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-        String loggedInEmail = (String) session.getAttribute("authUserEmail");
-        loggedInPrincipal = (Principals) principalsFacade.findByEmail(loggedInEmail).get(0);
+        //Choosing the user by the managedproperty key, if no such member throwing an error message
+        int principalID;
+        try {
+            principalID = Integer.parseInt(userID);
+            shownPrincipal = principalsFacade.find(principalID);
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Klaida: ", "ID formatas netinkamas."));
+            return;
+        }
+        if(shownPrincipal == null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Klaida: ", "Naudotojas su tokiu ID neegzistuoja."));
+            return;
+        }
         
-        firstname = loggedInPrincipal.getFirstname();
-        lastname = loggedInPrincipal.getLastname();
-        birthdate = loggedInPrincipal.getBirthdate();
-        email = loggedInPrincipal.getEmail();
-        phoneNumber = loggedInPrincipal.getPhonenumber();
-        address = loggedInPrincipal.getAddress();
-        about = loggedInPrincipal.getAbout();
+        //Setting to be shown fields
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");      
+        firstname = shownPrincipal.getFirstname();
+        lastname = shownPrincipal.getLastname();
+        birthdate = sdf.format(shownPrincipal.getBirthdate());
+        if(shownPrincipal.getMembershipuntill() == null) {
+            memberUntil = "Ne narys";
+        } else {
+            memberUntil = sdf.format(shownPrincipal.getMembershipuntill());
+        }
+        email = shownPrincipal.getEmail();
+        phoneNumber = shownPrincipal.getPhonenumber();
+        address = shownPrincipal.getAddress();
+        about = shownPrincipal.getAbout();
         if(about == null) {
             about = "";
         }
     }
-
+    
+    // Getters / setters -------------------------------------------------------
+    
     public String getFirstname() {
         return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
     }
 
     public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
     public String getBirthdate() {
         return birthdate;
-    }
-
-    public void setBirthdate(String birthdate) {
-        this.birthdate = birthdate;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getAbout() {
         return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
     }
 
     public boolean isAboutField() {
         return aboutField;
     }
 
-    public void setAboutField(boolean aboutField) {
-        this.aboutField = aboutField;
-    }
-
     public boolean isPictureField() {
         return pictureField;
+    }  
+
+    public String getMemberUntil() {
+        return memberUntil;
     }
 
-    public void setPictureField(boolean pictureField) {
-        this.pictureField = pictureField;
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
     
-    
+    public String getEmptyString() { 
+        return ""; 
+    }
     
 }
